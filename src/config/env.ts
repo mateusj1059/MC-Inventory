43449d082f4ce2f4ebfb3ac0308c2970e7b69337
mateusj1059 <mateusjhon2105@gmail.com>
2026-05-10 23:52:30 +0000

    feat: add inventory module and PUT endpoints

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,8 +3,8 @@ dotenv.config();
 
 export const env = {
   port: Number(process.env.PORT) || 3000,
-  mongoUri: process.env.MONGO_URI || "",
+  mongoUri: process.env.MONGO_URI || 'mongodb://jhoncito2105_db_user:B%40nfield2105@ac-tks3m6r-shard-00-00.zf6yqzr.mongodb.net:27017,ac-tks3m6r-shard-00-01.zf6yqzr.mongodb.net:27017,ac-tks3m6r-shard-00-02.zf6yqzr.mongodb.net:27017/?ssl=true&replicaSet=atlas-8rpwzd-shard-0&authSource=admin&appName=Project',
   mongoDbName: process.env.MONGO_DB_NAME || "minecraft_inventory",
-  jwtSecret: process.env.JWT_SECRET || "secret",
+  jwtSecret: process.env.JWT_SECRET || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30',
   jwtExpiration: process.env.JWT_EXPIRATION || "10h",
 };
